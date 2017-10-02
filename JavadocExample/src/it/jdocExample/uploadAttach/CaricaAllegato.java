250f738841906bad276503411cf7944205ffba3f
PCoronas90 <pie.coronas@gmail.com>
2017-10-02 16:28:07 +0200

    bug fix
    bug fix

--- JavadocExample/src/it/jdocExample/uploadAttach/CaricaAllegato.java
+++ JavadocExample/src/it/jdocExample/uploadAttach/CaricaAllegato.java
@@ -1,28 +1,3 @@
-package it.jdocExample.uploadAttach;
-import java.io.IOException;
-import java.io.PrintWriter;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.ext.Provider;
-
-import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.disk.DiskFileItemFactory;
-import org.apache.commons.fileupload.servlet.ServletFileUpload;
-import java.io.File;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.List;
-
-import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-
 /**
 La classe <code>CaricaAllegto</code> contiene un metodo che permette di salvare un file in una directory,
 specificata dalla variabile <code>Directory_Di_Upload</code>.
